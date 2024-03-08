a1aecee16a8aa213571d3753c53447cfb22e14c8
Ajay Pokharel <ajaypokharel8@gmail.com>
2024-03-07 20:49:02 -0600

    Initial Commit

---
+++ Task/TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskApp.swift
+//  Task
+//
+//  Created by Ajay Pokharel on 3/7/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
